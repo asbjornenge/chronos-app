6416ee4f3fde5d4bc929970f4f44b214fc3511e7
Asbjørn Enge <asbjorn@hanafjedle.net>
2018-12-06 14:51:58 +0100

    Basics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import Router from 'tiny-react-router'
+import Dashboard from './screens/Dashboard'
+import './App.css'
+
+let routes = {
+  '/' : Dashboard
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src="/graphics/logo.png" />
+          <img alt="logo" src="/graphics/logo.png" />
         </header>
+        <Router routes={routes} />
       </div>
     );
   }
