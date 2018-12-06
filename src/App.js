837417a51433a9e21997e7f4bcc42c691717b93b
Asbjørn Enge <asbjorn@hanafjedle.net>
2018-12-06 14:16:14 +0100

    Added logo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <img src="/graphics/logo.png" />
         </header>
       </div>
     );
