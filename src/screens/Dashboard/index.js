e7488e85fc8e936427166b1b35349b667dd06e2a
Asbjørn Enge <asbjorn@hanafjedle.net>
2018-12-07 11:32:46 +0100

    More chronos work

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import './index.css'
 
 export default class Dashboard extends Component {
   render() {
     return (
-      <h1>This be dashboard</h1>
+      <div className="Dashboard">
+        <div className="top">
+          <input type="search" placeholder="Name Contains" />
+          <div className="filterbuttons">
+            <div className="filterbutton first">
+              <img src="/graphics/flame.svg" alt="flame" />
+              <span>FAILING</span>
+            </div>
+            <div className="filterbutton">
+              <img src="/graphics/check.svg" alt="check" />
+              <span>PASSING</span>
+            </div>
+            <div className="filterbutton last">
+              <img src="/graphics/pause.svg" alt="pause" />
+              <span>PAUSED</span>
+            </div>
+          </div>
+        </div>        
+      </div>
     )
   }
 }
