1f164b2078f4cb4919f0c9d87bbf2e5bdf43cfa1
Asbjørn Enge <asbjorn@hanafjedle.net>
2018-12-07 10:49:56 +0100

    Moved header to comp

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import Router from 'tiny-react-router'
+import Header from './shared/components/Header'
 import Dashboard from './screens/Dashboard'
 import createStore from './store'
 import './index.css';
@@ -16,9 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img alt="logo" src="/graphics/logo.png" />
-        </header>
+        <Header />
         <Provider store={store}>
           <Router routes={routes} />
         </Provider>
